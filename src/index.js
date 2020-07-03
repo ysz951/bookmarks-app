4f13bdc6964607658454a83c9e0c09f90d55b94e
ysz951 <shengyang_zhou@baylor.edu>
2020-07-03 03:54:23 -0500

    console env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
+console.log(process.env)
 ReactDOM.render(
   <BrowserRouter>
     <App />
